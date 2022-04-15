1d8dabedf644d202ae27908e25a438ea7f03eac3
handou2 <2864466519@qq.com>
2022-04-15 10:59:38 +0800

    new

---
+++ src/components/sandbox/NewsRouter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserList from '../../views/sandbox/user-manage/UserList'
+// import axios from 'axios'
+// import { Switch, Route, Redirect } from 'react-router-dom'
+// import {Spin } from 'antd'
+// const LocalRouterMap = {
+//     "/user-manage/list":UserList,
+// }
+
+export default function NewsRouter() {
+    return (
+       <UserList></UserList>
+    )
+}
